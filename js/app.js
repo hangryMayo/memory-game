6a31ed02c29e70b8b0c87152ee80bef215bad2f8
Meo Huynh <mayoh727@gmail.com>
2018-07-02 14:09:18 -0700

    Create flipCard function to add class open and show
    Function adds a class of open and show. An event listener will be added to work together with this function.

--- js/app.js
+++ js/app.js
@@ -29,6 +29,11 @@ function generateBoard() {
   cardDeck.appendChild(fragment);
 }
 
+function flipCard() {
+  this.classList.add('open');
+  this.classList.add('show');
+}
+
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
