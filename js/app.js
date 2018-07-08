c333596b81a5332b9d2e8129faa4a0c6833799e7
Meo Huynh <mayoh727@gmail.com>
2018-07-08 14:15:33 -0700

    Create move counter function

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-/*
- * Create a list that holds all of your cards
- */
 const cardIcons = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-anchor", "fa-leaf", "fa-bicycle", "fa-diamond", "fa-bomb", "fa-leaf", "fa-bomb", "fa-bolt", "fa-bicycle", "fa-paper-plane-o", "fa-cube"];
 
 const cardDeck = document.querySelector('.deck');
@@ -9,15 +6,16 @@ let cardElements = document.getElementsByClassName('card');
 let allCards = Array.from(cardElements);
 let openCards = [];
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+// move variables
+const moveContainer = document.querySelector('.moves');
+let moves = 0;
 
 function generateBoard() {
   cardDeck.innerHTML = '';
+  // reset move count
+  moves = 0;
+  moveContainer.innerHTML = moves;
+
   shuffle(cardIcons);
   createDeck();
 }
@@ -61,6 +59,8 @@ function openList() {
   openCards.push(this);
 
   if (openCards.length === 2) {
+    moveCounter();
+
     if (openCards[0].innerHTML === openCards[1].innerHTML) {
       cardMatch();
       console.log("IT'S A MATCH");
@@ -94,6 +94,11 @@ function cardClash() {
   }, 600);
 }
 
+function moveCounter() {
+  moves++;
+  moveContainer.innerHTML = moves;
+}
+
 function resetGame(){
   allCards.forEach(function(card) {
     card.classList.remove('open', 'show');
@@ -101,17 +106,6 @@ function resetGame(){
 
   generateBoard();
 }
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
 
 window.onload = generateBoard();
 
