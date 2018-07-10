05e53b6c0654049c7a07a11c10ee3dd76c719c74
Meo Huynh <mayoh727@gmail.com>
2018-07-09 20:36:01 -0700

    Complete all game functionalites

--- js/app.js
+++ js/app.js
@@ -1,7 +1,29 @@
-const cardIcons = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-anchor", "fa-leaf", "fa-bicycle", "fa-diamond", "fa-bomb", "fa-leaf", "fa-bomb", "fa-bolt", "fa-bicycle", "fa-paper-plane-o", "fa-cube"];
+// array of all card icons
+const cardIcons = [
+  "fa-diamond",
+  "fa-paper-plane-o",
+  "fa-anchor",
+  "fa-bolt",
+  "fa-cube",
+  "fa-anchor",
+  "fa-leaf",
+  "fa-bicycle",
+  "fa-diamond",
+  "fa-bomb",
+  "fa-leaf",
+  "fa-bomb",
+  "fa-bolt",
+  "fa-bicycle",
+  "fa-paper-plane-o",
+  "fa-cube"
+];
 
+// li.card parent container
 const cardDeck = document.querySelector('.deck');
+
 const resetButton = document.querySelector('.restart');
+
+// card variables
 let cardElements = document.getElementsByClassName('card');
 let allCards = Array.from(cardElements);
 let openCards = [];
@@ -27,18 +49,21 @@ let timer;
 // modal variables
 const modal = document.querySelector('.modal');
 const playAgainButton = document.getElementById('play-again');
-const close = document.getElementById('close');
 
+/*
+* ALL FUNCTIONS
+*/
 function generateBoard() {
+  // clears board
   cardDeck.innerHTML = '';
   // reset match card
   matchCards = [];
-
+  // shuffles card's icons
   shuffle(cardIcons);
   createDeck();
   // reset timer
-  timerContainer.innerHTML = '00:00:00';
   clearInterval(timer);
+  timerContainer.innerHTML = '00:00:00';
   // reset move count
   moves = 0;
   moveContainer.innerHTML = moves;
@@ -47,19 +72,22 @@ function generateBoard() {
     stars[i].style.visibility = 'visible';
   }
 }
-
+// @description creates and assigns cards with icons and adds listener for cards
 function createDeck() {
   const fragment = document.createDocumentFragment();
-
+  // @description creates & appends li, i elements to hold cardIcons
   for (let i = 0; i < 16; i++) {
     const newLi = document.createElement('li');
     newLi.classList.add('card');
-    newLi.addEventListener('click', flipCard);
-    newLi.addEventListener('click', openList);
 
     const newI = document.createElement('i');
     newI.classList.add('fa');
     newI.classList.add(cardIcons[i]);
+
+    // listens for clicks on all li.cards
+    newLi.addEventListener('click', flipCard);
+    newLi.addEventListener('click', openList);
+    newLi.addEventListener('click', openModal);
 
     newLi.appendChild(newI);
     fragment.appendChild(newLi);
@@ -67,7 +95,6 @@ function createDeck() {
 
   cardDeck.appendChild(fragment);
 }
-
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -82,12 +109,25 @@ function shuffle(array) {
 
     return array;
 }
-
+// @description disables clicks while matching is occuring
+function invalidChoice() {
+  for (i = 0; i < cardElements.length; i++) {
+    cardElements[i].classList.add('disable');
+  };
+}
+// @description clears invalidChoice function so cards can be clicked on again
+function validChoice() {
+  for (i = 0; i < cardElements.length; i++) {
+    cardElements[i].classList.remove('disable');
+  };
+}
+// @description push current set of cards to openCards array & checks for matches
 function openList() {
   openCards.push(this);
 
   if (openCards.length === 2) {
     moveCounter();
+    invalidChoice();
 
     if (openCards[0].innerHTML === openCards[1].innerHTML) {
       cardMatch();
@@ -98,15 +138,14 @@ function openList() {
     }
   }
 }
-
+// @description adds class open and show to reveal cards
 function flipCard() {
-  this.classList.add('open');
-  this.classList.add('show');
+  this.classList.add('open', 'show', 'disable');
 }
-
+// @description adds class of match and removes class of open and show
 function cardMatch() {
-  openCards[0].classList.add('match');
-  openCards[1].classList.add('match');
+  openCards[0].classList.add('match', 'disable');
+  openCards[1].classList.add('match', 'disable');
 
   matchCards.push(openCards[0]);
   matchCards.push(openCards[1]);
@@ -115,16 +154,19 @@ function cardMatch() {
   openCards[1].classList.remove('open', 'show');
 
   openCards = [];
+  validChoice();
 }
-
+// @description removes class open and show; openCards array reset to empty
 function cardClash() {
   setTimeout(function() {
-    openCards[0].classList.remove('open', 'show');
-    openCards[1].classList.remove('open', 'show');
+    openCards[0].classList.remove('open', 'show', 'disable');
+    openCards[1].classList.remove('open', 'show', 'disable');
+
     openCards = [];
+    validChoice();
   }, 600);
 }
-
+// @description starts timer
 function timerStart() {
   seconds = 0, minutes = 0;
   timer = setInterval(function(){
@@ -139,7 +181,7 @@ function timerStart() {
         }
     }, 1000);
 }
-
+// @description increments move by 1 and adjusts star ratings
 function moveCounter() {
   moves++;
   moveContainer.innerHTML = moves;
@@ -148,42 +190,45 @@ function moveCounter() {
     timerStart();
   }
   // star rating  based on moves
-  if (moves > 8 && moves <= 16) {
+  if (moves > 13 && moves <= 20) {
     stars[2].style.visibility = 'hidden';
-  } else if (moves > 16) {
+  } else if (moves > 20) {
     stars[2].style.visibility = 'hidden';
     stars[1].style.visibility = 'hidden';
   }
 }
-
+// @description resets game
 function resetGame(){
+  // @description flips all opened cards over
   allCards.forEach(function(card) {
     card.classList.remove('open', 'show');
   });
 
   generateBoard();
 }
-
-window.onload = generateBoard();
-
-resetButton.addEventListener('click', resetGame);
-
+// @description opens congratulations modal
 function openModal() {
-  if (matchCards.length == 2) {
+  if (matchCards.length == 16) {
     clearInterval(timer);
     totalTime.innerHTML = timerContainer.innerHTML;
-    // add a class of show
+
+    modal.classList.add('show');
 
     totalMoves.innerHTML = moves;
     totalStars.innerHTML = starContainer.innerHTML;
 
+    playAgain();
   }
 }
-
-function closeModal() {
-
-}
-
+// @description new game when play again button is clicked on
 function playAgain() {
-
+  playAgainButton.addEventListener('click', function() {
+    modal.classList.remove('show');
+    generateBoard();
+  });
 }
+
+// @description generates/shuffles board when window loads/refreshes
+window.onload = generateBoard();
+// listens for reset button
+resetButton.addEventListener('click', resetGame);
